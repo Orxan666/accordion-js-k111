7c25ff34573d9aa7dbf3b886c1032befa74c301b
orxan666 <orxanallahyarov03@gmail.com>
2022-06-26 12:43:10 +0400

    aco-js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// var arr = ["Ugur", "Orxan", "Lale", 50];
+// var myObj = {
+//   id: 1,
+//   ad: "Eli",
+//   hobbies: ["futbol", "tenis", "cudo"],
+
+// };
+// myObj.ad = "Cavid";
+// console.log(myObj.hobbies[0]);
+// // arr.shift()
+// console.log(arr);
+
+// // var Age=25
+// // console.log(Age);
+
+// var arrayList=[{id:1,name:"Orxan"},{id:2,name:"Lale"}]
+// arrayList.push("5")
+// console.log(arrayList);
+
+var h1=document.querySelector(".txt")
+h1.onclick=function(){
+    this.style.color="red"
+}
+h1.onclick=function(){
+    this.style.background="green"
+}
+h1.onclick=function(){
+    this.style.fontSize="250px"
+}
